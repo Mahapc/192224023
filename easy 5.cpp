dd11fad98f799596b098e80a96e3b5b26c574dec
Mahalakshmi <122218175+Mahapc@users.noreply.github.com>
2023-02-17 08:14:18 +0530

    Add files via upload

--- easy 5.cpp
+++ easy 5.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
+#include<conio.h>
+#include<math.h>
 int main()
 {
-	float number,square,cube;
-	printf("enter the decimal number:");
-	scanf("%f",&number);
-	square=number*number;
-	cube=number*number*number;
-	printf("the square of number=%.2f\n",square);
-	printf("the cube of number=%.3f",cube);
+	float inpu,s,c;
+	printf("Enter a number you want to square and cube : ");
+	scanf("%f",&inpu);
+	printf("Square of given number is : %f\n",inpu*inpu);
+	printf("Cube of given number is %f",inpu*inpu*inpu);
 }
